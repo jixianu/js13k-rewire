e504c4c643dd9731874c565ebbf475e3379a583f
王琦 <13331043275@163.com>
2019-05-20 13:35:55 +0800

    add some message

--- src/space.ts
+++ src/space.ts
@@ -1,38 +1,40 @@
 type System = {
-    addEntity: (entity: Entity) => void;
-    update?: (time: number) => void;
-    shutdown?: () => void;
-};
-type RenderSystem = System & { render: (context: Context, time: number) => void }
+  addEntity: (entity: Entity) => void
+  update?: (time: number) => void
+  shutdown?: () => void
+}
+type RenderSystem = System & {
+  render: (context: Context, time: number) => void
+}
 type UpdateSystem = System & { update: (time: number) => void }
 
 interface Space {
-    entities: Partial<Entity>[];
+  entities: Partial<Entity>[]
 
-    registerSystem(system: System): void;
+  registerSystem(system: System): void
 
-    addEntity(entity: Partial<Entity>): void;
+  addEntity(entity: Partial<Entity>): void
 
-    shutdown(): void;
+  shutdown(): void
 }
 
 const createSpace = (): Space => {
-    const systems: System[] = [];
-    const entities: Partial<Entity>[] = [];
+  const systems: System[] = []
+  const entities: Partial<Entity>[] = []
 
-    return {
-        registerSystem: (system: System) => {
-            systems.push(system);
-        },
-        addEntity: (entity: Partial<Entity>) => {
-            entities.push(entity);
-            systems.forEach(system => {
-                system.addEntity(entity as Entity);
-            });
-        },
-        shutdown:()=> {
-            systems.forEach(system => system.shutdown && system.shutdown());
-        },
-        entities
-    };
-};
+  return {
+    registerSystem: (system: System) => {
+      systems.push(system)
+    },
+    addEntity: (entity: Partial<Entity>) => {
+      entities.push(entity)
+      systems.forEach(system => {
+        system.addEntity(entity as Entity)
+      })
+    },
+    shutdown: () => {
+      systems.forEach(system => system.shutdown && system.shutdown())
+    },
+    entities
+  }
+}
--- src/start.ts
+++ src/start.ts
@@ -1,143 +1,153 @@
 /// <reference path="types.ts" />
-/// <reference path="utils" />
+/// <reference path="utils.ts" />
 /// <reference path="math-util.ts" />
 /// <reference path="html.ts" />
 /// <reference path="resources.ts" />
 /// <reference path="game.ts" />
 
 const showEndScreen = () => {
-    nextMsg.innerHTML = 'Thanks for playing!';
-    nextBtn.innerHTML = 'AGAIN';
-    showElement(levelDoneElement, () => {
-        nextBtn.addEventListener('click', e => {
-            location.reload();
-        });
-    });
-    saveLevel(0);
-};
+  nextMsg.innerHTML = 'Thanks for playing!'
+  nextBtn.innerHTML = 'AGAIN'
+  showElement(levelDoneElement, () => {
+    nextBtn.addEventListener('click', e => {
+      location.reload()
+    })
+  })
+  saveLevel(0)
+}
 
-const startGame = (parent: HTMLElement, resources: Resources, startLevel: number) => {
-    const game = createGame();
-    let currentLevel = startLevel;
+const startGame = (
+  parent: HTMLElement,
+  resources: Resources,
+  startLevel: number
+) => {
+  const game = createGame()
+  let currentLevel = startLevel
 
-    const startNextLevel = () => {
-        console.log('start level ' + currentLevel);
+  const startNextLevel = () => {
+    console.log('start level ' + currentLevel)
 
-        let tutorial: HTMLElement;
-        if (currentLevel == 0) {
-            tutorial = resources.tutorial1;
-            gameElement.appendChild(tutorial);
-            showElement(tutorial);
+    let tutorial: HTMLElement
+    if (currentLevel == 0) {
+      tutorial = resources.tutorial1
+      gameElement.appendChild(tutorial)
+      showElement(tutorial)
+    }
+    if (currentLevel == 2) {
+      tutorial = resources.tutorial2
+      gameElement.appendChild(tutorial)
+      showElement(tutorial)
+    }
+
+    const level = game.createLevel(
+      gameData.levels[currentLevel],
+      resources,
+      () => {
+        if (tutorial) {
+          hideElement(tutorial, () => {
+            removeElement(tutorial)
+          })
         }
-        if (currentLevel == 2) {
-            tutorial = resources.tutorial2;
-            gameElement.appendChild(tutorial);
-            showElement(tutorial);
+        if (currentLevel < gameData.levels.length - 1) {
+          currentLevel++
+          saveLevel(currentLevel)
+          hideElement(resetElement)
+          showElement([levelDoneElement], () => {
+            nextBtn.onclick = () => {
+              nextBtn.onclick = null
+              hideElement(
+                [levelDoneElement, level.canvas, levelInfo, nodeInfo],
+                () => {
+                  removeElement(level.canvas)
+                  startNextLevel()
+                }
+              )
+            }
+          })
+        } else {
+          showEndScreen()
         }
+      }
+    )
 
-        const level = game.createLevel(gameData.levels[currentLevel], resources, () => {
-            if (tutorial) {
-                hideElement(tutorial, () => {
-                    removeElement(tutorial);
-                });
-            }
-            if (currentLevel < gameData.levels.length - 1) {
-                currentLevel++;
-                saveLevel(currentLevel);
-                hideElement(resetElement);
-                showElement([levelDoneElement], () => {
-                    nextBtn.onclick = () => {
-                        nextBtn.onclick = null;
-                        hideElement([levelDoneElement, level.canvas, levelInfo, nodeInfo], () => {
-                            removeElement(level.canvas);
-                            startNextLevel();
-                        });
+    parent.appendChild(level.canvas)
+    levelInfo.innerHTML = 'Level ' + (currentLevel + 1)
+    showElement([level.canvas, resetElement, levelInfo, nodeInfo])
 
-                    };
-                });
-            } else {
-                showEndScreen();
-            }
-        });
+    const resetLevel = () => {
+      if (tutorial) {
+        hideElement(tutorial, () => {
+          removeElement(tutorial)
+        })
+      }
+      backBtn.onclick = skipBtn.onclick = resetBtn.onclick = null
+      hideElement([level.canvas, resetElement, levelInfo, nodeInfo], () => {
+        level.shutdown()
+        removeElement(level.canvas)
+        startNextLevel()
+      })
+    }
 
-        parent.appendChild(level.canvas);
-        levelInfo.innerHTML = 'Level ' + (currentLevel + 1);
-        showElement([level.canvas, resetElement, levelInfo, nodeInfo]);
+    resetBtn.onclick = resetLevel
+    skipBtn.onclick = () => {
+      if (currentLevel > gameData.levels.length - 2) {
+        return
+      }
+      currentLevel++
+      resetLevel()
+    }
+    backBtn.onclick = () => {
+      if (currentLevel < 1) {
+        return
+      }
+      currentLevel--
+      resetLevel()
+    }
+  }
 
-        const resetLevel = () => {
-            if (tutorial) {
-                hideElement(tutorial, () => {
-                    removeElement(tutorial);
-                });
-            }
-            backBtn.onclick = skipBtn.onclick = resetBtn.onclick = null;
-            hideElement([level.canvas, resetElement, levelInfo, nodeInfo], () => {
-                level.shutdown();
-                removeElement(level.canvas);
-                startNextLevel();
-            });
-
-        };
-
-        resetBtn.onclick = resetLevel;
-        skipBtn.onclick = () => {
-            if (currentLevel > gameData.levels.length - 2) {
-                return;
-            }
-            currentLevel++;
-            resetLevel();
-        };
-        backBtn.onclick = () => {
-            if (currentLevel < 1) {
-                return;
-            }
-            currentLevel--;
-            resetLevel();
-        };
-    };
-
-    startNextLevel();
-};
+  startNextLevel()
+}
 
 const prepareGame = () => {
-    const [loadingBar, context] = createCanvas(200, 7);
-    loadingBar.id = 'loadingbar';
-    loadingElement.appendChild(loadingBar);
-    showElement(loadingBar);
-    context.strokeStyle = 'grey';
-    context.fillStyle = 'grey';
-    context.lineWidth = 1;
+  const [loadingBar, context] = createCanvas(200, 7)
+  loadingBar.id = 'loadingbar'
+  loadingElement.appendChild(loadingBar)
+  showElement(loadingBar)
+  context.strokeStyle = 'grey'
+  context.fillStyle = 'grey'
+  context.lineWidth = 1
 
-    context.strokeRect(0.5, 0.5, 199, 4);
-    generateResources(p => {
-        context.fillRect(0.5, 0.5, 199 / 100 * p, 4);
-    }, (resources) => {
+  context.strokeRect(0.5, 0.5, 199, 4)
+  generateResources(
+    p => {
+      context.fillRect(0.5, 0.5, (199 / 100) * p, 4)
+    },
+    resources => {
+      hideElement(loadingBar, () => {
+        showElement([menuElement, descriptionElement])
 
-        hideElement(loadingBar, () => {
-            showElement([menuElement, descriptionElement]);
+        const savedLevel = loadLevel()
+        continueBtn.style.visibility = savedLevel ? 'visible' : 'hidden'
 
-            const savedLevel = loadLevel();
-            continueBtn.style.visibility = savedLevel ? 'visible' : 'hidden';
+        const hideUIandStartGame = (startLevel: number) => {
+          startBtn.onclick = continueBtn.onclick = null
+          hideElement([titleElement, menuElement, descriptionElement], () => {
+            startGame(contentElement, resources, startLevel)
+          })
+        }
+        startBtn.onclick = () => {
+          saveLevel(0)
+          hideUIandStartGame(0)
+        }
 
-            const hideUIandStartGame = (startLevel: number) => {
-                startBtn.onclick = continueBtn.onclick = null;
-                hideElement([titleElement, menuElement, descriptionElement], () => {
-                    startGame(contentElement, resources, startLevel);
-                });
-            };
-            startBtn.onclick = () => {
-                saveLevel(0);
-                hideUIandStartGame(0);
-            };
+        continueBtn.onclick = () => {
+          hideUIandStartGame(savedLevel)
+        }
 
-            continueBtn.onclick = () => {
-                hideUIandStartGame(savedLevel);
-            };
+        // hideUIandStartGame(10); // skip main menu and start with level
+      })
+    }
+  )
+}
 
-            // hideUIandStartGame(10); // skip main menu and start with level
-        });
-
-    });
-};
-
-showElement(titleElement, prepareGame);
+showElement(titleElement, prepareGame)
